834ea49a729ed076186efeedfb1ebad95d3a3351
zaynahw <zaynahwahab@gmail.com>
2025-11-12 19:08:06 -0500

    Intial Commit

---
+++ testproject/testprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testprojectApp.swift
+//  testproject
+//
+//  Created by Zaynah Wahab on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct testprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
